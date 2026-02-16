0a45132cc65e7d16f65e010c7d4ddcd92739a763
Steel_Mastr <cqtj08@gmail.com>
2026-02-16 21:16:02 +0100

    inizio sito

---
+++ .history/src/database_testo_20260216190942.js
@@ -0,0 +1,65 @@
+const traduzioni = {
+    "it": {
+        titolo: "Vieni a conoscere Oliva gessi!",
+        home: "Home",
+        about: "Chi siamo",
+        contact: "Contatti"
+    },
+    "en": {
+        titolo: "Get to know Oliva gessi!",
+        home: "Home",
+        about: "About us",
+        contact: "Contact"
+    }
+}
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                DO NOT TOUCH                                */
+/* -------------------------------------------------------------------------- */
+var state =  getDefaultLang();
+
+function aggiornaInterfaccia() {
+    const elementi = document.querySelectorAll('[data-key]');
+    
+    elementi.forEach(el => {
+        const chiave = el.getAttribute('data-key');
+        const testoTradotto = traduzioni[state][chiave];
+        
+        if (testoTradotto) {
+            el.textContent = testoTradotto;
+        }
+    });
+}
+
+/* -------------------------------------------------------------------------- */
+/*                            Cambiamenti di stato                            */
+/* -------------------------------------------------------------------------- */
+function getDefaultLang() {
+    if(navigator.language.substring(0, 2) === "en")
+        return "en";
+
+    return "it";
+}
+function defaultState() {
+    state = getDefaultLang();
+    aggiornaInterfaccia();
+    return true;
+}
+function changeStateTo(_state) {
+    if(_state !== "it" && _state !== "en") {
+        defaultState();
+        return;
+    }
+    state = _state;
+    aggiornaInterfaccia();
+}
+function changeState() {
+    if(state === "it") 
+        changeStateTo("en");
+    else
+        changeStateTo("it");
+}
+
+document.addEventListener("DOMContentLoaded", aggiornaInterfaccia);
