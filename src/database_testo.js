eba528beab567e42af7b57d68c04ed6c4e82027f
Steel_Mastr <cqtj08@gmail.com>
2026-04-02 19:05:30 +0200

    Integrazione del nuovo testo

--- src/database_testo.js
+++ src/database_testo.js
@@ -20,17 +20,27 @@ const traduzioni = {
         testo_2: "",
         title_3: "Luoghi storici",
         testo_3: "Tra le colline dell’Oltrepò Pavese, Oliva Gessi è un borgo dalla storia estremamente ricca, nato attorno a un castello già attestato nel Medioevo e citato nei registri inquisitoriali del XIII secolo.<br>" +
-                "Tra il Seicento e l’Ottocento gli Isimbardi trasformarono il paese in un centro rurale armonioso. " +
-                "Qui si intrecciano vicende come i casi inquisitoriali di Ottone Villano e il rogo di Benvenuta, accanto alla figura di Luigi Versiglia.<br>" +
-                "Il paese conserva simboli identitari come il Voltò, il giardino all’italiana, la chiesa di San Martino (riedificata nel 1682) e il teatro parrocchiale del 1926, cuore della vita comunitaria.<br>" +
-                "Intorno, le colline vitate e i terreni gessosi, sfruttati fin dall’età romana e valorizzati anche da Abramo De Benedetti, raccontano una tradizione millenaria.<br>",
+            "Tra il Seicento e l’Ottocento gli Isimbardi trasformarono il paese in un centro rurale armonioso. " +
+            "Qui si intrecciano vicende come i casi inquisitoriali di Ottone Villano e il rogo di Benvenuta, accanto alla figura di Luigi Versiglia.<br>" +
+            "Il paese conserva simboli identitari come il Voltò, il giardino all’italiana, la chiesa di San Martino (riedificata nel 1682) e il teatro parrocchiale del 1926, cuore della vita comunitaria.<br>" +
+            "Intorno, le colline vitate e i terreni gessosi, sfruttati fin dall’età romana e valorizzati anche da Abramo De Benedetti, raccontano una tradizione millenaria.<br>",
         title_4: "Personaggi storici",
-        testo_4: "Oliva Gessi affonda le sue origini nel X secolo con Ottone I di Sassonia, che dopo aver consolidato il potere imperiale e rafforzato il legame con la Chiesa, nel 972 donò il territorio alla nuora Teofano, segnandone la prima attestazione e l’ingresso nei beni ecclesiastici.<br>" +
-                "Nei secoli successivi il borgo passò sotto varie dominazioni, tra cui i Belcredi, influenti nell’Oltrepò Pavese grazie a incarichi politici e proprietà, e gli Isimbardi, che trasformarono il castello in residenza e svilupparono l’agricoltura fino alla loro estinzione nel Novecento.<br>" +
-                "Il paese diede i natali a Luigi Versiglia, martire in Cina nel 1930. Tra Otto e Novecento Abramo De Benedetti introdusse innovazioni, ma la sua esperienza si concluse con la morte del figlio nella Seconda guerra mondiale.<br>",
+        testo_4: "Tra i personaggi fondamentali di Oliva Gessi vi è la famiglia Isimbardi, che trasformò il castello in residenza signorile e sviluppò l’economia agricola fino alla sua estinzione nel Novecento.<br>" +
+            "In questo contesto nacque Luigi Versiglia nel 1873, poi missionario in Cina e martire nel 1930.<br>" +
+            "Tra Otto e Novecento Abramo De Benedetti modernizzò la tenuta, ma la sua esperienza fu segnata dalla tragedia del figlio Ugo, deportato e ucciso ad Auschwitz nel 1943 durante la Seconda guerra mondiale.",
         /* About us */
         about_team_title: "Il team Custodi del Tempo",
-        about_team: "Descrizione del team", // TODO
+        about_team: "Descrizione del team",
+        title_1_about:"Jacopo Cicuti",
+        testo_1_about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus, lorem tincidunt finibus pellentesque, tellus ex porttitor purus, vel egestas erat metus et turpis. Nunc laoreet ante enim, vel elementum justo fringilla at. Fusce id ex non risus euismod auctor. In vitae accumsan nisi. Proin venenatis iaculis risus, a hendrerit nisl facilisis non. Nam convallis nibh et est lobortis auctor. Vestibulum neque nisl, finibus sed dapibus sit amet, varius sit amet ex. Nullam dapibus vehicula orci, id sodales libero volutpat vel. Vivamus nisi leo, mattis ut est convallis, pharetra ornare ipsum. Fusce elementum velit at massa mattis rutrum. Curabitur tincidunt consectetur nibh, in molestie urna ullamcorper sed. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
+        title_2_about:"Cristian Frizzarin",
+        testo_2_about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus, lorem tincidunt finibus pellentesque, tellus ex porttitor purus, vel egestas erat metus et turpis. Nunc laoreet ante enim, vel elementum justo fringilla at. Fusce id ex non risus euismod auctor. In vitae accumsan nisi. Proin venenatis iaculis risus, a hendrerit nisl facilisis non. Nam convallis nibh et est lobortis auctor. Vestibulum neque nisl, finibus sed dapibus sit amet, varius sit amet ex. Nullam dapibus vehicula orci, id sodales libero volutpat vel. Vivamus nisi leo, mattis ut est convallis, pharetra ornare ipsum. Fusce elementum velit at massa mattis rutrum. Curabitur tincidunt consectetur nibh, in molestie urna ullamcorper sed. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
+        title_3_about:"Fabio Pistore",
+        testo_3_about: "",
+        title_4_about:"Mohamed Aziz Bouajila",
+        testo_4_about: "",
+        title_5_about:"Mariem Bouajila",
+        testo_5_about: "",
         /* Impostazioni */
         cambio_colore: "Cambia tema:",
         sys_default: "Default",
@@ -60,14 +70,16 @@ const traduzioni = {
             "The first written testimony of the existence of Oliva is in 972, when emperor Otto I gifted the town to his daughter-in-law, who will donate it to a monastery. In the period of fiefs, Oliva was part of Montalto’s reign, switching under the control of different families, of which in particular we remember the Isimbardi family.",
         title_2: "The castle",
         testo_2: "",
-        title_3: "Historical places",
+        title_3: "Historical landmarks",
         testo_3: "Among the hills of Oltrepò Pavese, Oliva Gessi is a village with a very rich history, built around a castle already attested in the Middle Ages and mentioned in the 13th-century inquisitorial registers.<br>" +
             "Between the seventeenth and nineteenth centuries the Isimbardi family turned the town into a harmonious rural centre. " +
             "Here, events such as the inquisitorial cases of Otto Villano and the Benvenuta fire intertwine, alongside the figure of Luigi Versiglia.<br>" +
             "The town preserves symbols of identity such as the Voltò, the Italian garden, the church of San Martino (rebuilt in 1682) and the parish theatre from 1926, the heart of community life.<br>" +
             "Around it, the vineyard hills and chalky soils, exploited since Roman times and also enhanced by Abramo De Benedetti, tell of a thousand-year-old tradition.",
         title_4: "Historical characters",
-        testo_4: "",
+        testo_4: "One of the most important families of Oliva Gessi is the Isimbardi family, who transformed the castle into a noble residence and developed the agricultural economy of the village until the end of the family in the 1900s.<br>" +
+            "In this context, in 1873, another fundamental character was born: Luigi Versiglia, who became a missionary in China and eventually a martyr in 1930.<br>" +
+            "Between the 19th and the 20th centuries Abramo De Benedetti modernized the estate, but his experience was marked by the tragedy of his son Ugo, deported and killed in Auschwitz in 1943 during the Second World War.",
         /* About us */
         about_team_title: "The Custodi del Tempo team",
         about_team: "Team's description",
